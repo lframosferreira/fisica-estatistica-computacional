dbc08ce36ecc05161c672f226282c2d72bc472c9
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-05-11 14:47:21 -0300

    [NEW] less mc steps and comparison with christofides

--- simulated_annealing_tsp/competicao.py
+++ simulated_annealing_tsp/competicao.py
@@ -33,27 +33,35 @@ def plot_distance_temperature(distances: npt.NDArray[np.float_], temperatures: n
     plt.show()
 
 @jit(nopython=True)
-def performe_monte_carlo_steps(number_of_monte_carlo_steps: np.int_, number_of_cities: np.int_, temperature: np.float_, graph: npt.NDArray[np.float_], current_path: npt.NDArray[np.int_], current_path_cost: np.float_) -> np.float_:
+def performe_monte_carlo_steps(number_of_monte_carlo_steps: np.int_, number_of_cities: np.int_, temperature: np.float_, graph: npt.NDArray[np.float_], nodes: npt.NDArray[np.float_], current_path: npt.NDArray[np.int_], current_path_cost: np.float_) -> np.float_:
     for _ in range(number_of_monte_carlo_steps):
-        proposed_x, proposed_y = np.random.choice(np.arange(number_of_cities), size=2, replace=False)
-        proposed_path: npt.NDArray[np.int_] = current_path.copy()
-        proposed_path[proposed_x], proposed_path[proposed_y] = proposed_path[proposed_y], proposed_path[proposed_x]
-        decrease: np.float_ = graph[current_path[proposed_x - 1]][current_path[proposed_x]] + graph[current_path[proposed_x]][current_path[(proposed_x + 1) % number_of_cities]] + graph[current_path[proposed_y - 1]][current_path[proposed_y]] + graph[current_path[proposed_y]][current_path[(proposed_y + 1) % number_of_cities]]
-        increase: np.float_ = graph[proposed_path[proposed_x - 1]][proposed_path[proposed_x]] + graph[proposed_path[proposed_x]][proposed_path[(proposed_x + 1) % number_of_cities]] + graph[proposed_path[proposed_y - 1]][proposed_path[proposed_y]] + graph[proposed_path[proposed_y]][proposed_path[(proposed_y + 1) % number_of_cities]]
-        proposed_path_cost: np.float_ = current_path_cost - decrease + increase
-        delta: np.float_ = proposed_path_cost - current_path_cost
-        r: np.float_ = np.random.rand()
-        P: np.float_ = np.exp(-1 * delta / temperature)
-        if delta < 0 or r <= P:
-            current_path = proposed_path.copy()
-            current_path_cost = proposed_path_cost
+        for _ in range(number_of_cities):
+            proposed_x, proposed_y = np.sort(np.random.choice(np.arange(number_of_cities), size=2, replace=False))
+            proposed_path: npt.NDArray[np.int_] = current_path.copy()
+            # proposed_path[proposed_x], proposed_path[proposed_y] = proposed_path[proposed_y], proposed_path[proposed_x]
+            proposed_path[proposed_x + 1: proposed_y] = np.flip(proposed_path[proposed_x + 1: proposed_y])
+
+            remove_edge_1_cost: np.float_ = graph[current_path[proposed_x]][current_path[(proposed_x + 1) % number_of_cities]]
+            remove_edge_2_cost: np.float_ = graph[current_path[proposed_y - 1]][current_path[proposed_y]]
+            add_edge_1_cost: np.float_ = graph[proposed_path[proposed_x]][proposed_path[(proposed_x + 1) % number_of_cities]]
+            add_edge_2_cost: np.float_ = graph[proposed_path[proposed_y - 1]][proposed_path[proposed_y]]
+
+            decrease: np.float_ = remove_edge_1_cost + remove_edge_2_cost
+            increase: np.float_ = add_edge_1_cost + add_edge_2_cost
+            proposed_path_cost: np.float_ = current_path_cost - decrease + increase
+            delta: np.float_ = proposed_path_cost - current_path_cost
+            r: np.float_ = np.random.rand()
+            P: np.float_ = np.exp(-1 * delta / temperature)
+            if delta < 0 or r <= P:
+                current_path = proposed_path.copy()
+                current_path_cost = proposed_path_cost
     return current_path, current_path_cost
 
 
 def tsp(number_of_cities: np.int_, temperature: np.float_, delta_t: np.float_, temperature_inferior_limit: np.float_) -> npt.NDArray[np.int_]:
-    number_of_monte_carlo_steps: np.int_ = 100 # hard coded
+    number_of_monte_carlo_steps: np.int_ = 1 # hard coded
     initial_temperature: np.float_ = temperature
-    graph, nodes = generate_random_graph(number_of_nodes=number_of_cities)
+    graph, nodes = GRAPH, NODES
     current_path: npt.NDArray[np.int_] = np.arange(number_of_cities)
     np.random.shuffle(current_path)
     edges: npt.NDArray[np.int_] = np.append(np.lib.stride_tricks.sliding_window_view(current_path, 2), [[current_path[-1], current_path[0]]], axis=0)
@@ -65,7 +73,7 @@ def tsp(number_of_cities: np.int_, temperature: np.float_, delta_t: np.float_, t
     while temperature > temperature_inferior_limit:
         temperatures.append(temperature)
         distances.append(current_path_cost)
-        current_path, current_path_cost = performe_monte_carlo_steps(number_of_monte_carlo_steps=number_of_monte_carlo_steps, number_of_cities=number_of_cities, temperature=temperature, graph=graph, current_path=current_path, current_path_cost=current_path_cost)
+        current_path, current_path_cost = performe_monte_carlo_steps(number_of_monte_carlo_steps=number_of_monte_carlo_steps, number_of_cities=number_of_cities, temperature=temperature, graph=graph, nodes=nodes, current_path=current_path, current_path_cost=current_path_cost)
         temperature *= delta_t
 
     current_path = np.append(current_path, current_path[0])
